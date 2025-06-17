f7cea63f1593ff716c96b2009e6486103e40e9c9
Ding-Han Sean Yan <sean@dhsy.me>
2025-06-17 14:15:47 +0800

    feat: basic server boilerplate logic

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+// // Routes
+// const userRoutes = require('./routes/userRoutes');
+// app.use('/users', userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("hello world!");
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
