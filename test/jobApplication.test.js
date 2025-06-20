9cf580c3cbf8f87f0f68f98175854d232092c344
Ding-Han Sean Yan <sean@dhsy.me>
2025-06-20 12:03:44 +0800

    test: Job Application GET route testing

--- test/jobApplication.test.js
+++ test/jobApplication.test.js
@@ -33,38 +33,39 @@ describe("/", () => {
   })
 })
 
-// describe('POST /applications', () => {
-//   test('should create a job application with valid data', async () => {
-//     const res = await request(app).post('/applications').send({
-//       company: 'OpenAI',
-//       title: 'AI Engineer',
-//       status: 'applied'
-//     });
-//
-//     expect(res.statusCode).toBe(200);
-//     // expect(res.body).toHaveProperty('_id');
-//     // expect(res.body.company).toBe('OpenAI');
-//   });
-//
-  // it('should return 400 if required fields are missing', async () => {
-  //   const res = await request(app).post('/api/jobapplications').send({
-  //     title: 'AI Engineer'
-  //   });
-  //
-  //   expect(res.statusCode).toBe(400);
-  //   expect(res.body.error).toMatch(/company/i);
-  // });
-  //
-  // it('should return 400 for invalid status value', async () => {
-  //   const res = await request(app).post('/api/jobapplications').send({
-  //     company: 'OpenAI',
-  //     title: 'AI Engineer',
-  //     status: 'invalid_status'
-  //   });
-  //
-  //   expect(res.statusCode).toBe(403);
-  // });
-// });
+describe('POST /applications', () => {
+  test('should create a job application with valid data', async () => {
+    const res = await request(app).post('/applications').send({
+      company: 'OpenAI',
+      title: 'AI Engineer',
+      status: 'applied'
+    });
+
+    expect(res.statusCode).toBe(200);
+    // expect(res.body).toHaveProperty('_id');
+    // expect(res.body.company).toBe('OpenAI');
+  });
+
+  test('should return 400 if required fields are missing', async () => {
+    const res = await request(app).post('/applications').send({
+      title: 'AI Engineer'
+    });
+
+    expect(res.statusCode).toBe(400);
+    expect(res.body.error).toMatch(/company/i);
+  });
+
+  test('should return 400 for invalid status value', async () => {
+    const res = await request(app).post('/applications').send({
+      company: 'OpenAI',
+      title: 'AI Engineer',
+      status: 'invalid_status'
+    });
+
+    expect(res.statusCode).toBe(400);
+    expect(res.body.error).toMatch(/status/i);
+  });
+});
 
 // describe('GET /api/jobapplications', () => {
 //   it('should retrieve all job applications', async () => {
