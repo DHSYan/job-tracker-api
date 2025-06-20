e668801561f64a35540614d32b2313b6f43c0ec2
Ding-Han Sean Yan <sean@dhsy.me>
2025-06-20 11:11:53 +0800

    refactor: extract server logic from app

--- app.js
+++ app.js
@@ -10,18 +10,20 @@ const app = express();
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+if (process.env.NODE_ENV !== 'test') {
+  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(() => console.log('MongoDB connected'))
+    .catch(err => console.error(err));
+}
 
 app.use('/applications', jobRouter);
 
 app.get("/", (_, res) => {
-  res.send("hello world!");
+  res.status(200).send("hello world!");
 })
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;
+
 
